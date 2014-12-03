aba9dd3cdef4c923109d85365896015f8c49f734
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 18:03:56 +0100

    Creamos el metodo cuentaVocales que estara synchronized y nos contara el numero de vocales dependiendo del char recibido.

--- repaso3/Contador.java
+++ repaso3/Contador.java
@@ -14,4 +14,25 @@ public class Contador
     // Definimos una variable de tipo String que tendra nuestra frase a analizar.
     String frase = "en un lugar de la mancha que todos conocemos aqui";
     
+    public Contador()
+    {
+        
+    }
+    
+    public synchronized int cuentaVocales(char vocal)
+    {
+        int contador = 0;
+        for(int i = 0; i < frase.length(); i++)
+        {
+            
+            if(frase.charAt(i) == vocal)
+            {
+                contador = contador + 1;
+            }
+            
+        }
+        
+        return contador;
+    }
+    
 }
