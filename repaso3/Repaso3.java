d7b45820c56cc439bd58d52def6275eb18ac0daf
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 12:17:09 +0100

    Iniciamos nuestro proyecto.

---
+++ repaso3/Repaso3.java
@@ -0,0 +1,22 @@
+/*
+ * Crea una clase Java que utilice 5 hilos pcomún que representa el número de vocales totales.
+ * Para contar el número de vocales que hay en un determinado texto
+ * (que puede ser introducido por teclado o estar en un fichero).
+ * Cada hilo se encargará de contar una vocal diferente, actualizando todos los hilos una misma variable
+ */
+package repaso3;
+
+/**
+ *
+ * @author findag
+ */
+public class Repaso3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
