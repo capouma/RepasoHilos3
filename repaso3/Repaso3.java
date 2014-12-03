ba93999a2f1603b723668b8cfd6dea43f3ea432c
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 16:56:58 +0100

    Creamos la clase Hilo que extendera de Thread.

--- repaso3/Repaso3.java
+++ repaso3/Repaso3.java
@@ -15,8 +15,25 @@ public class Repaso3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        // Definimos una variable de tipo String que tendra nuestra frase a analizar.
+        String frase = "En un lugar de la mancha que todos conocemos aqui";
+        
+        // Creamos los 5 hilos que usaremos para contar las vocales de nuestra texto/archivo.
+        Hilo hilo1 = new Hilo("Hilo 1 es A", frase);
+        Hilo hilo2 = new Hilo("Hilo 2 es E", frase);
+        Hilo hilo3 = new Hilo("Hilo 3 es I", frase);
+        Hilo hilo4 = new Hilo("Hilo 4 es O", frase);
+        Hilo hilo5 = new Hilo("Hilo 5 es U", frase);
+        
+        // Lanzamos nuestros hilos.
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        hilo4.start();
+        hilo5.start();
+        
     }
     
 }
