049fc28167494414effcc6f7c2afa3e941d18627
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 17:48:33 +0100

    Creamos la clase contador. Esta clase tendra el texto a analizar

---
+++ repaso3/Contador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repaso3;
+
+/**
+ *
+ * @author findag
+ */
+public class Contador
+{
+    // Definimos una variable de tipo String que tendra nuestra frase a analizar.
+    String frase = "en un lugar de la mancha que todos conocemos aqui";
+    
+}
